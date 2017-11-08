9890325d12090c5b02448376a32907590b3e56da
dasorita <dasolhannah@gmail.com>
2017-11-08 21:21:14 +0900

    game객체 설계함

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-/**
- * Created by hannahch on 2017-11-08.
- */
 for(var i='a'.charCodeAt(0); i<='z'.charCodeAt(0); i++){
     var keys = document.getElementById('keys');
     var button = document.createElement('button');
@@ -9,8 +6,52 @@ for(var i='a'.charCodeAt(0); i<='z'.charCodeAt(0); i++){
     button.setAttribute('class','alphaBtn');
 }
 
-
 keys.onclick = function () {
     var val = event.target.innerHTML;
     game.checkAnswer(val);
+};
+
+// function Game(options) {
+//     this.life = options.life;
+//     this.count = count;
+//
+// }
+//
+//
+// var game1 = new Game({life: 5, count:0});
+
+var game = {
+    life: 5,
+    count: 0,
+    answer: null,
+    hint: null,
+    guess: [],
+
+    init: function() {
+
+    },
+    correct: function() {
+
+    },
+    incorrect: function() {
+
+    },
+    checkAnswer: function (val) {
+
+    },
+    showResult: function () {
+
+    },
+    isOver: function () {
+
+    },
+    isClear: function () {
+
+    },
+    clear: function () {
+
+    },
+    gameOver: function () {
+
+    }
 };
